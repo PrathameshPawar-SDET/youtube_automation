228ce85d5db289ba0eaf6e3ab6ec8a4e79ec351a
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2024-08-12 23:28:50 +0530

    Testcase01

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,13 +1,18 @@
 package demo;
 
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
@@ -20,17 +25,54 @@ import java.time.Duration;
 import java.util.logging.Level;
 
 import demo.utils.ExcelDataProvider;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases extends ExcelDataProvider{ // Lets us read the data
-        ChromeDriver driver;
+        WebDriver driver;
+
+        @FindBy(xpath = "//div[@id=\"guide-links-primary\"]//a[text()='About']")
+        private WebElement about;
+
+        @FindBy(css = "section:nth-child(1) > p:nth-child(2)")
+        private  WebElement abouttext;
 
         /*
          * TODO: Write your tests here with testng @Test annotation.
          * Follow `testCase01` `testCase02`... format or what is provided in
          * instructions
          */
+
+        @Test
+        public void testCase01(){
+                System.out.println("Starting with testCase 01");
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+                driver.get("https://www.youtube.com/");
+
+                SoftAssert SA = new SoftAssert();
+                SA.assertEquals(driver.getCurrentUrl(),"https://www.youtube.com/","URL Did Not Match!");
+                try{
+                        Wrappers.click(driver, about);
+
+                        wait.until(ExpectedConditions.visibilityOf(abouttext));
+                        String aboutsectiontext = abouttext.getText();
+                        System.out.println("Message on About us Page : " +aboutsectiontext);
+
+
+                }catch (Exception e){
+                        e.printStackTrace();
+                }
+
+                SA.assertAll();
+
+        }
+
+        @Test
+        public void testCase02(){
+
+        }
 
         /*
          * Do not change the provided methods unless necessary, they will help in
@@ -41,7 +83,7 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
                 System.setProperty("java.util.logging.config.file", "logging.properties");
 
                 // NOT NEEDED FOR SELENIUM MANAGER
-                // WebDriverManager.chromedriver().timeout(30).setup();
+                WebDriverManager.chromedriver().timeout(30).setup();
 
                 ChromeOptions options = new ChromeOptions();
                 LoggingPreferences logs = new LoggingPreferences();
@@ -56,12 +98,13 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
                 driver = new ChromeDriver(options);
 
                 driver.manage().window().maximize();
+                PageFactory.initElements(new AjaxElementLocatorFactory(driver,5), this);
         }
 
         @AfterTest
         public void endTest() {
-                driver.close();
-                driver.quit();
+//                driver.close();
+//                driver.quit();
 
         }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -14,4 +15,42 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    WebDriver driver;
+
+    public static boolean click(WebDriver driver, WebElement element){
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.elementToBeClickable(element));
+            Actions act = new Actions(driver);
+            act.moveToElement(element);
+            Thread.sleep(5000);
+            act.click(element).perform();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            try{
+                element.click();
+                return true;
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+
+    public static boolean sendkeys(WebDriver driver, WebElement inputbox, String keystosend){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOf(inputbox));
+        try {
+            inputbox.sendKeys(keystosend);
+            return true;
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return false;
+
+    }
 }
